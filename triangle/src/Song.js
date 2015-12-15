c35c6d29887856e5dcda57acc136d4a51869a39a
Tentello <prempaolo@gmail.com>
2015-12-15 15:08:31 +0100

    sprint1

--- triangle/src/Song.js
+++ triangle/src/Song.js
@@ -3,5 +3,12 @@ function Song() {
 
 Song.prototype.persistFavoriteStatus = function(value) {
   // something complicated
+
+
+
+  //mi piace commentare
+
+  //sprint1
+
   throw new Error("not yet implemented");
 };
